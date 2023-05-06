22af02b44800f6555c080910310ecbe3958b96d1
Alex Kravchenko <alexkravchenko1605@gmail.com>
2023-05-06 14:36:39 +0300

    feat:  solve part 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const fs = require("fs");
+
+const filePath = path.join(__dirname, "text.txt");
+const readStream = fs.createReadStream(filePath, "utf-8");
+
+let result = " ";
+
+readStream.on("data", (chunk) => {
+  result += chunk;
+});
+
+readStream.on("end", () => {
+  console.log(result);
+});
